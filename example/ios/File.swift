4ba626a051dcc0fc7bdeafa38388e1c48f44d178
A389216 <aurelien.viale@socgen.com>
2023-08-23 15:42:24 +0200

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  AndroidSmsRetrieverExample
+//
+
+import Foundation
---
+++ ios/AndroidSmsRetriever.swift
@@ -0,0 +1,8 @@
+@objc(AndroidSmsRetriever)
+class AndroidSmsRetriever: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}
